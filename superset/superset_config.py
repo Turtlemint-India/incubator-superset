c0876afa301a5ca4df37ed7cf2d4a13ac4fbb129
saurabh1920 <56253846+saurabh1920@users.noreply.github.com>
2020-08-31 21:42:20 +0530

    changed redis url

--- superset/superset_config.py
+++ superset/superset_config.py
@@ -5,11 +5,14 @@ CACHE_CONFIG = {
     'CACHE_TYPE': 'redis',
     'CACHE_DEFAULT_TIMEOUT': 60 * 60 * 24, # 1 day default (in secs)
     'CACHE_KEY_PREFIX': 'superset_results',
-    'CACHE_REDIS_URL': 'redis://172.18.0.3:6379/0',
+    'CACHE_REDIS_URL': 'redis://localhost:6379/0',
 }
 
+# python 3.6 required
 #To start a Celery worker to leverage the configuration run:
 # celery worker --app=superset.tasks.celery_app:app --pool=prefork -O fair -c 4
 
 # To start a job which schedules periodic background jobs, run
 # celery beat --app=superset.tasks.celery_app:app
+
+# export SUPERSET_CONFIG_PATH="/home/turtle/saurabh_ws/env_superset1/lib/python3.6/site-packages/superset/superset_config.py"
