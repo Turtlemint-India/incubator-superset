14d51b73bc65d93ab99b0d4209fa4a3b4bf9a796
Keshav Saini <keshav.s@turtlemint.com>
2020-11-10 15:56:05 +0530

    enable Google OAuth

---
+++ superset/custom_sso_security_manager.py
@@ -0,0 +1,52 @@
+import logging
+
+from superset import SupersetSecurityManager
+
+logger = logging.getLogger(__name__)
+
+
+class CustomSsoSecurityManager(SupersetSecurityManager):
+
+    def auth_user_oauth(self, userinfo):
+        """
+            OAuth user Authentication
+
+            :userinfo: dict with user information the keys have the same name
+            as User model columns.
+        """
+        # Search for the user by email to support existing users logging in via their Google Account
+        if "email" in userinfo:
+            user = self.find_user(email=userinfo["email"])
+        else:
+            logger.error("User info does not have username or email {0}".format(userinfo))
+            return None
+        # User is disabled
+        if user and not user.is_active:
+            from flask_appbuilder.const import LOGMSG_WAR_SEC_LOGIN_FAILED
+            logger.info(LOGMSG_WAR_SEC_LOGIN_FAILED.format(userinfo))
+            return None
+        # If user does not exist on the DB and not self user registration, go away
+        if not user and not self.auth_user_registration:
+            return None
+        # User does not exist, create one if self registration.
+        if not user:
+            role_name = self.auth_user_registration_role
+            print()
+            if self.auth_user_registration_role_jmespath:
+                import jmespath
+
+                role_name = jmespath.search(
+                    self.auth_user_registration_role_jmespath, userinfo
+                )
+            user = self.add_user(
+                username=userinfo["username"],
+                first_name=userinfo.get("first_name", ""),
+                last_name=userinfo.get("last_name", ""),
+                email=userinfo.get("email", ""),
+                role=self.find_role(role_name),
+            )
+            if not user:
+                logger.error("Error creating a new OAuth user %s" % userinfo["username"])
+                return None
+        self.update_user_auth_stat(user)
+        return user
--- superset/superset_config.py
+++ superset/superset_config.py
@@ -1,11 +1,35 @@
 import os
+
+from flask_appbuilder.const import AUTH_OAUTH
+
+from superset.custom_sso_security_manager import CustomSsoSecurityManager
+
 SQLALCHEMY_DATABASE_URI = os.environ['SQLALCHEMY_DATABASE_URI']
 CACHE_WARMUP_USER = "whopper"
 CACHE_DEFAULT_TIMEOUT = 60 * 60 * 6
 CACHE_CONFIG = {
     'CACHE_TYPE': 'redis',
-    'CACHE_DEFAULT_TIMEOUT': 60 * 60 * 6, # 6 hr default (in secs)
+    'CACHE_DEFAULT_TIMEOUT': 60 * 60 * 6,  # 6 hr default (in secs)
     'CACHE_KEY_PREFIX': 'superset_results',
     'CACHE_REDIS_URL': 'redis://localhost:6379/0',
 }
 
+CUSTOM_SECURITY_MANAGER = CustomSsoSecurityManager
+AUTH_TYPE = AUTH_OAUTH
+AUTH_USER_REGISTRATION = True
+AUTH_USER_REGISTRATION_ROLE = "Public"
+
+OAUTH_PROVIDERS = [
+    {'name': 'google', 'icon': 'fa-google', 'token_key': 'access_token', 'whitelist': ['@turtlemint.com'],
+     'remote_app': {
+         'client_id': '78989321337-5e44ugm9ev8davgp7591njjv7o81naoc.apps.googleusercontent.com',
+         'client_secret': '-ul9faMKxh5ddwmXrchaUewr',
+         'api_base_url': 'https://www.googleapis.com/oauth2/v2/',
+         'client_kwargs': {
+             'scope': 'email profile'
+         },
+         'request_token_url': None,
+         'access_token_url': 'https://accounts.google.com/o/oauth2/token',
+         'authorize_url': 'https://accounts.google.com/o/oauth2/auth'}
+     }
+]
