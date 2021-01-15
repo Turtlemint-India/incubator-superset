6ab97771823308eb267984fe3a4139cdfb2c9848
Keshav Saini <keshav.s@turtlemint.com>
2021-01-15 18:50:38 +0530

    change entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#
+
 #initialize_superset () {
 #    USER_COUNT=$(fabmanager list-users --app superset | awk '/email/ {print}' | wc -l)
 #    if [ "$?" ==  0 ] && [ $USER_COUNT == 0 ]; then
@@ -20,21 +20,21 @@
 #        echo Apache-Superset Already Initialized.
 #    fi
 #}
-#
-#superset db upgrade
-#echo Container deployment type "$NODE_TYPE"
-#if [ "$NODE_TYPE" == "server" ]; then
-#  gunicorn -w 5 -k gevent --timeout 180 -b 0.0.0.0:$SUPERSET_WEBSERVER_PORT --limit-request-line 0 --limit-request-field_size 0 "superset.app:create_app()" &
-#elif [ "$NODE_TYPE" == "worker" ]; then
-#  celery worker --app=superset.tasks.celery_app:app --pool=prefork -O fair -c 4 &
-#  celery beat --app=superset.tasks.celery_app:app &
-#else
-#  echo Invalid node type "$NODE_TYPE"
-#  exit 1
-#fi
-#
-#
-#
+
+superset db upgrade
+echo Container deployment type "$NODE_TYPE"
+if [ $NODE_TYPE = "server" ]; then
+  gunicorn -w 5 -k gevent --timeout 180 -b 0.0.0.0:$SUPERSET_WEBSERVER_PORT --limit-request-line 0 --limit-request-field_size 0 "superset.app:create_app()"
+elif [ $NODE_TYPE = "worker" ]; then
+  celery worker --app=superset.tasks.celery_app:app --pool=prefork -O fair -c 4 &
+  celery beat --app=superset.tasks.celery_app:app &
+else
+  echo Invalid node type "$NODE_TYPE"
+  exit 1
+fi
 
 
-gunicorn -w 5 -k gevent --timeout 180 -b 0.0.0.0:$SUPERSET_WEBSERVER_PORT --limit-request-line 0 --limit-request-field_size 0 "superset.app:create_app()" &
+
+
+
+#gunicorn -w 5 -k gevent --timeout 180 -b 0.0.0.0:$SUPERSET_WEBSERVER_PORT --limit-request-line 0 --limit-request-field_size 0 "superset.app:create_app()" &
