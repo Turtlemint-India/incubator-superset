01c3916f2a540eec85c3b57325c797e815d1857c
saurabh1920 <56253846+saurabh1920@users.noreply.github.com>
2021-03-01 10:27:32 +0530

    added sharding domain

--- superset/superset_config.py
+++ superset/superset_config.py
@@ -95,3 +95,5 @@ RESULTS_BACKEND = RedisCache(
 SUPERSET_WEBSERVER_PROTOCOL = os.environ['SUPERSET_WEBSERVER_PROTOCOL']
 SUPERSET_WEBSERVER_ADDRESS = os.environ['SUPERSET_WEBSERVER_ADDRESS']
 SUPERSET_WEBSERVER_PORT = os.environ['SUPERSET_WEBSERVER_PORT']
+
+SUPERSET_WEBSERVER_DOMAINS = ['service.prismd.stage.mintpro.in', 'service1.prismd.stage.mintpro.in', 'service2.prismd.stage.mintpro.in']
