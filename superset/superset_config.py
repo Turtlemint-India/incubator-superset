894d11907a59976f698dc433e4224952f6ce36e3
saurabh1920 <56253846+saurabh1920@users.noreply.github.com>
2021-03-03 19:57:53 +0530

    Update superset_config.py

--- superset/superset_config.py
+++ superset/superset_config.py
@@ -96,7 +96,7 @@ SUPERSET_WEBSERVER_PROTOCOL = os.environ['SUPERSET_WEBSERVER_PROTOCOL']
 SUPERSET_WEBSERVER_ADDRESS = os.environ['SUPERSET_WEBSERVER_ADDRESS']
 SUPERSET_WEBSERVER_PORT = os.environ['SUPERSET_WEBSERVER_PORT']
 
-SUPERSET_WEBSERVER_DOMAINS = ['www.prismd.stage.mintpro.in', 'www.service.prismd.stage.mintpro.in']
+SUPERSET_WEBSERVER_DOMAINS = ['https://www.prismd.stage.mintpro.in', 'https://www.service.prismd.stage.mintpro.in', 'https://www.service1.prismd.stage.mintpro.in', 'https://www.service2.prismd.stage.mintpro.in']
 ENABLE_CORS = True
 HTTP_HEADERS = {}
 CORS_OPTIONS = {
